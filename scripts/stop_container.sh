38c638aa27e677d431d310604543af120105ddc5
puneethnani <puneethnani56@gmail.com>
2025-09-11 00:50:45 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,4 +2,10 @@
 set -e
 
 # Stop the running container (if any)
-echo "Hiiii"
+echo "Stopping old container..."
+CONTAINER_ID=$(docker ps -q --filter "publish=5000")
+if [ ! -z "$CONTAINER_ID" ]; then
+    docker stop $CONTAINER_ID
+    docker rm $CONTAINER_ID
+fi
+
